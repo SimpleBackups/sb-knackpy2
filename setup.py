2a72fc5c4b38e2ba332cc21435bac8ed08628dfc
sb-nour <nour@simplebackups.com>
2023-12-26 19:47:45 +0200

    Update dev.txt and setup.py versions

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def build_config(env, readme="README.md"):
         "packages": ["knackpy"],
         "tests_require": ["pytest", "coverage"],
         "url": "http://https://github.com/SimpleBackups/sb-knackpy",
-        "version": "1.2.73",
+        "version": "1.2.74",
     }
 
 
